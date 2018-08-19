bafe6e7b203fbf55db7285f4c20fc624f4080e2f
tipsysquid <ken@tipsysquid.io>
2018-08-19 01:15:42 -0700

    Converted 3 endpoints to standard functions and now call them from the configureBilling endpoint.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,29 +15,33 @@ wyre_keys.baseUrl = 'https://api.testwyre.com';
 const wyre = new WyreClient(wyre_keys);
 import { WyreWallet, IClientWyreWallet, IWyreWallet } from "./lib/WyreWallet";
 import { IPaymentMethod } from './lib/PaymentMethod';
+import { create } from 'domain';
 
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send("Welcome to Gilded OnRamp!");
 });
 
-export const wyreRates = functions.https.onRequest((request, response) => {
-    
-    wyre.get("/rates", {}, {
-        timeout: 1500
-    })
-    .then((rates) => {
-        response.send(rates);
-    })
-    .catch((error) => {
-        response.status(500).send(error);
-    })
+export const wyreRates = functions.https.onRequest(async (request, response) => {
+
+    return await testRates(request, response);
 
 });
 
-export const createWallet = functions.https.onRequest(async (request, response) => {
-    //console.log(JSON.stringify(request));
-    const client_wallet = request.body;
+function testRates(req, res) {
+    wyre.get("/rates", {}, {
+        timeout:1500
+    })
+    .then((rates) => {
+        res.send(rates);
+    })
+    .catch((error) => {
+        res.status(500).send(error);
+    })
+}
+
+async function createWallet(req_wallet)  {
+    const client_wallet = req_wallet;
 
     // @TODO use type
     // const new_wallet: IClientWyreWallet = {
@@ -53,10 +57,15 @@ export const createWallet = functions.https.onRequest(async (request, response) 
     let wallet = <IWyreWallet> {};
     try {
         wallet = await wyre.post('/wallets', new_wallet)
-        //response.send(wallet);
     }
     catch (error) {
-        response.status(500).send("Unable to create wallet with Wyre: " + JSON.stringify(error));
+        console.log(error);
+        const resp = {
+            status:500,
+            message:"Unable to create wallet with Wyre",
+            ...error
+        }
+        throw resp;
     }
 
     // @TODO optimize this round trip
@@ -65,18 +74,22 @@ export const createWallet = functions.https.onRequest(async (request, response) 
         const ref_wallet = await db.collection('wallets').doc(wallet.id);
         const doc_wallet = await ref_wallet.get();
         const data_id = doc_wallet.data().id;
-        response.send(data_id);   
+        return data_id;
     }
     catch (error) {
-        response.status(500).send("Error saving wallet to db! Error: " + error);
+        const resp = {
+            ...error,
+            message: "Error saving wallet to db!",
+            status: 500
+        };
+        throw resp;
     }
     
     
-});
+}
 
-export const createPaymentMethod = functions.https.onRequest(async (request, response) => {
-    const client_paymentMethod = request.body;
-    //console.log(client_paymentMethod);
+async function createPaymentMethod(req_pm)  {
+    const client_paymentMethod = req_pm;
 
     // @TODO validate client_paymentMethod
     const new_paymentMethod = client_paymentMethod;
@@ -85,28 +98,37 @@ export const createPaymentMethod = functions.https.onRequest(async (request, res
     let paymentMethod = <IPaymentMethod>{};
     try {
         paymentMethod = await wyre.post('/paymentMethods', new_paymentMethod);
-    //console.log(paymentMethod);
     }
     catch (error) {
-        response.status(500).send("Unable to create new paymentMethod. Error: " + JSON.stringify(error));
-    }
-    
+        console.log(error);
+        const resp = {
+            ...error,
+            message: "Unable to crea,te new paymentMethod.",
+            status: 500
+        };
+        throw resp;
+    }    
 
     try {
         const set_paymentMethod = await db.collection('paymentMethods').doc(paymentMethod.id).set(paymentMethod);
         const ref_paymentMethod = await db.collection('paymentMethods').doc(paymentMethod.id);
         const doc_paymentMethod = await ref_paymentMethod.get();
         const data_id = doc_paymentMethod.data().id;
-        response.status(200).send("Payment method created: "+ data_id);
+        return data_id;
     }
     catch (error) {
-        response.status(500).send("Unable to save paymentMethod in db! :" + error);
+        const resp = {
+            ...error,
+            message: "Unable to save paymentMethod to db!",
+            status: 500
+        };
+        throw resp;
     }
 
-})
+}
 
-export const createTransfer = functions.https.onRequest(async (req, res) => {
-    const client_transfer = req.body;
+async function createTransfer(req_createTransfer) {
+    const client_transfer = req_createTransfer;
     // @TODO: validate transfer
 
     // @TODO: SAVE CALLBACKURL
@@ -126,12 +148,58 @@ export const createTransfer = functions.https.onRequest(async (req, res) => {
     let transfer;
     try {
         transfer = await wyre.post('/transfer', new_transfer)
-        res.status(200).send({ id: transfer.id, status: transfer.status } );
+        return transfer;
     }
     catch (error) {
-        res.status(500).send("Unable to create transfer: " + error);
+        console.log(error);
+        const resp = {
+            ...error,
+            message: "Unable to create transfer",
+            status: 500
+        };
+        throw resp;
     }
 
+}
+/**
+ * Hits createWallet, createPaymentMethod, and createTransfer 
+ * where createTransfer is not a quote.
+ */
+export const configureBilling = functions.https.onRequest(async (req, res) => {
+    let client_wallet;
+    let client_paymentMethod;
+    let client_transfer;
+    try {
+        client_wallet = req.body.wallet;
+        client_paymentMethod = req.body.paymentMethod;
+        client_transfer = req.body.transfer;
+    }
+    catch (error) {
+        const resp = {
+            ...error,
+            message: "Unable to declare variables",
+            status:500
+        }
+        res.status(resp.status).send(resp);
+    }
+
+    // @TODO determine real variables here from client as well as how to properly populate them
+    try {
+        const wallet_id = await createWallet(client_wallet);
+        client_paymentMethod.owner = "wallet: " + wallet_id;
+        const paymentMethod = await createPaymentMethod(client_paymentMethod);
+        const transfer = await createTransfer(client_transfer);
+        res.status(200).send(transfer);
+    }
+    catch (error) {
+        console.log(typeof error);
+        const resp = {
+            err:error,
+            message: "Unable to configure billing",
+            status: 500
+        };
+        res.status(resp.status).send(resp);
+    }
 })
 
 export const confirmTransfer = functions.https.onRequest(async (req, res) => {
