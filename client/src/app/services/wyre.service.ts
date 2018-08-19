d748d2ac582bb0f67b9288317ca7c606e8ba0bc7
Gil Hildebrand <gilhildebrand@gmail.com>
2018-08-19 01:57:42 -0500

    Wyre service

--- client/src/app/services/wyre.service.ts
+++ client/src/app/services/wyre.service.ts
@@ -1,23 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'environments/environment';
+import { FormService } from './form.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WyreService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private formService: FormService) { }
 
   createWallet() {
     // const url = environment.api_url + `/sendEmail`;
+
+    const start = this.formService.startForm.value;
+    const basic = this.formService.basicForm.value;
+    const billing = this.formService.billingForm.value;
+
+    const body = JSON.stringify({
+      start, basic, billing
+    });
 
     const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
     });
-
-    // const body = JSON.stringify({ to: this.email, subject: this.subject, html: this.htmlBody });
 
     // call cloud service
     // return this.http.post(url, body, { headers: httpHeaders }).toPromise();
